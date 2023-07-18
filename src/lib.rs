4b0938de385c3254cd27a02559fa68acfb21029e
Mihir Gadgil <mmgadgil20@gmail.com>
2023-07-17 18:13:38 -0600

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,75 @@
+use rand::{prelude::SliceRandom, Rng};
+
+#[cfg(feature = "standard-deck")]
+pub mod standard_deck;
+
+/// A deck of cards.
+#[derive(Debug)]
+pub struct Deck<C> {
+    cards: Vec<C>,
+}
+
+impl<C: PartialEq> PartialEq for Deck<C> {
+    fn eq(&self, other: &Self) -> bool {
+        self.cards == other.cards
+    }
+}
+
+impl<C: PartialEq> Eq for Deck<C> {}
+
+impl<C> From<Vec<C>> for Deck<C> {
+    fn from(cards: Vec<C>) -> Self {
+        Deck::new(cards)
+    }
+}
+
+impl<C> Deck<C> {
+    /// Create a new deck with the given `cards` and the random number generator
+    /// `rng`.
+    pub fn new(cards: Vec<C>) -> Self {
+        Self { cards }
+    }
+
+    /// Create an empty deck with optionally provided `rng`.
+    pub fn new_empty() -> Self {
+        Self { cards: vec![] }
+    }
+
+    /// Add a `card` to the deck.
+    pub fn add(&mut self, card: C) {
+        self.cards.push(card);
+    }
+
+    /// Draw a card from the deck or `None` if the deck is empty.
+    pub fn draw(&mut self) -> Option<C> {
+        self.cards.pop()
+    }
+
+    /// Shuffle the deck.
+    pub fn shuffle<R: Rng>(&mut self, rng: &mut R) {
+        self.cards.shuffle(rng);
+    }
+
+    /// Sort the deck with the given comparator.
+    pub fn sort_by<F>(&mut self, compare: F)
+    where
+        F: FnMut(&C, &C) -> std::cmp::Ordering,
+    {
+        self.cards.sort_by(compare)
+    }
+
+    /// Create an `Iterator` over the cards of this deck.
+    pub fn iter(&self) -> impl Iterator<Item = &C> {
+        self.cards.iter()
+    }
+}
+
+impl<C> IntoIterator for Deck<C> {
+    type Item = C;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    /// Create an iterator over the `Card`s in the deck.
+    fn into_iter(self) -> Self::IntoIter {
+        self.cards.into_iter()
+    }
+}
---
+++ src/standard_deck.rs
@@ -0,0 +1,289 @@
+use std::collections::HashSet;
+
+use crate::Deck;
+
+/// A playing card.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Card {
+    Normal { suit: Suit, rank: Rank },
+    Wildcard,
+}
+
+impl Card {
+    /// Creat a "normal" playing card with the given `suit` and `rank`.
+    pub fn new_normal(suit: Suit, rank: Rank) -> Self {
+        Card::Normal { suit, rank }
+    }
+
+    /// Create a wildcard playing ... card.
+    ///
+    /// Also known as a Joker.
+    pub fn new_wildcard() -> Self {
+        Card::Wildcard
+    }
+
+    /// Get the `Rank` of the card if it is a normal card, `None` otherwise.
+    pub fn rank(&self) -> Option<&Rank> {
+        match self {
+            Card::Normal { rank, .. } => Some(rank),
+            Card::Wildcard => None,
+        }
+    }
+
+    /// Get the `Suit` of the card if it is a normal card, `None` otherwise.
+    pub fn suit(&self) -> Option<&Suit> {
+        match self {
+            Card::Normal { suit, .. } => Some(suit),
+            Card::Wildcard => None,
+        }
+    }
+}
+
+/// Suits of the "normal" playing cards.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Suit {
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades,
+}
+
+impl Suit {
+    /// Get all suits as a `Vec`.
+    pub fn all_suits() -> [Suit; 4] {
+        [Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades]
+    }
+}
+
+/// Ranks of the "normal" playing cards.
+///
+/// Numeric are from 2 to 10 (inclusive) along with Jack, Queen, King and Ace.
+/// Ordering is not imposed on the ranks, which rank is smaller or bigger should
+/// be decided by the caller.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Rank {
+    Numeric(u8),
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+impl Rank {
+    /// Create a new `Rank` from a numeric value, where 2..=10 correspond to the
+    /// respective ranks, 1, 11, 12, 13 correspond to the Ace, the Jack, the
+    /// Queen and the King respectively.
+    ///
+    /// # Panics
+    /// Panics if the provided value is not in the above specified range.
+    ///
+    /// # Example
+    /// ```rust
+    /// use card_deck::standard_deck::Rank;
+    ///
+    /// let rank = Rank::new(11);
+    /// assert_eq!(rank, Rank::Jack);
+    /// ```
+    pub fn new(value: u8) -> Rank {
+        match value {
+            0 | 14.. => panic!("rank value not in range"),
+            2..=10 => Rank::Numeric(value),
+            1 => Rank::Ace,
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+        }
+    }
+
+    /// Get all ranks.
+    pub fn all_ranks() -> HashSet<Rank> {
+        (1..=13).map(|value| Rank::new(value)).collect()
+    }
+
+    /// Get all numeric ranks.
+    pub fn all_numeric() -> HashSet<Rank> {
+        (2..=10).map(|value| Rank::new(value)).collect()
+    }
+
+    /// Get all face ranks, this includes [`Rank::Ace`].
+    pub fn all_face() -> HashSet<Rank> {
+        HashSet::from([Rank::Jack, Rank::Queen, Rank::King, Rank::Ace])
+    }
+
+    /// Get the "value" of a `Rank`.
+    ///
+    /// # Example
+    /// ```rust
+    /// use card_deck::standard_deck::Rank;
+    ///
+    /// let rank = Rank::new(11);
+    /// assert_eq!(rank.value(), 11);
+    /// ```
+    pub fn value(&self) -> u8 {
+        match self {
+            Rank::Numeric(val) => *val,
+            Rank::Jack => 11,
+            Rank::Queen => 12,
+            Rank::King => 13,
+            Rank::Ace => 1,
+        }
+    }
+}
+
+/// A builder for creating decks of standard cards.
+///
+/// Which cards to be included or excluded can be specified. Only one of
+/// `include` or `exclude` can be specified. Setting either one will nullify the
+/// other.
+///
+/// The default deck has 13 cards in each of the 4 suits and no wildcards
+/// for a total of 52 cards. The suits are Clubs, Diamonds, Hearts and
+/// Spades. The thirteen ranks are 2 to 10 (inclusive), Jack, Queen, King
+/// and Ace.
+///
+/// Number of repetitions of the basic deck can be specified using `subdecks`
+/// (default 1). This does not include wildcards. Wildcards are added after the
+/// rest of the deck has been built.
+///
+/// Number of wildcards can be specified using `wildcards` (default 0).
+pub struct StandardDeckBuilder {
+    // "Normal" cards to include
+    include: Option<HashSet<Card>>,
+    // "Normal" cards to exclude
+    // Must specify only one of `include` and `exclude`
+    exclude: Option<HashSet<Card>>,
+    // Repetitions of the deck
+    num_subdecks: Option<usize>,
+    // Number of wildcards to include in the deck
+    num_wildcards: Option<usize>,
+}
+
+impl StandardDeckBuilder {
+    /// Create the default deck builder.
+    pub fn new() -> StandardDeckBuilder {
+        Self {
+            include: None,
+            exclude: None,
+            num_subdecks: Some(1),
+            num_wildcards: Some(0),
+        }
+    }
+
+    /// Cards to include in the deck.
+    ///
+    /// Will set exclude to `None`.
+    pub fn include_cards(mut self, cards: HashSet<Card>) -> StandardDeckBuilder {
+        self.include = Some(cards);
+        self.exclude = None;
+        self
+    }
+
+    /// Cards to exclude from the deck.
+    ///
+    /// Will set include to `None`.
+    pub fn exclude_cards(mut self, cards: HashSet<Card>) -> StandardDeckBuilder {
+        self.exclude = Some(cards);
+        self.include = None;
+        self
+    }
+
+    /// Set repetitions of the basic deck.
+    ///
+    /// This does not include repetition of wildcards. Wildcards are added after
+    /// the rest of the deck has been built.
+    ///
+    /// # Panics
+    /// Panics if the repetitions are not a positive integer.
+    pub fn subdecks(mut self, num: usize) -> StandardDeckBuilder {
+        assert!(num >= 1, "num must be a positive integer");
+        self.num_subdecks = Some(num);
+        self
+    }
+
+    /// Set the number of wildcards to include in the deck.
+    ///
+    /// These are added after the rest of the deck has been built and as such
+    /// are not duplicated with the rest of the deck even if subdecks > 1.
+    pub fn wildcards(mut self, num: usize) -> StandardDeckBuilder {
+        self.num_wildcards = Some(num);
+        self
+    }
+
+    /// Build the deck with the selected options.
+    pub fn build(self) -> Deck<Card> {
+        let basic_deck = match (self.include, self.exclude) {
+            (Some(cards), None) => cards,
+            (None, Some(cards)) => full_deck().difference(&cards).cloned().collect(),
+            (None, None) => full_deck(),
+            (Some(_), Some(_)) => unreachable!("construction constraints do not allow this state"),
+        };
+        let mut deck: Vec<_> = if let Some(n) = self.num_subdecks {
+            basic_deck
+                .into_iter()
+                .flat_map(|card| std::iter::repeat(card).take(n))
+                .collect()
+        } else {
+            basic_deck.into_iter().collect()
+        };
+        if let Some(n) = self.num_wildcards {
+            (0..n).for_each(|_| deck.push(Card::new_wildcard()));
+        }
+        Deck::new(deck)
+    }
+}
+
+impl Default for StandardDeckBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+fn cards_for_suit(suit: &Suit) -> HashSet<Card> {
+    (1..=13u8)
+        .map(|rank| Card::new_normal(*suit, Rank::new(rank)))
+        .collect()
+}
+
+fn full_deck() -> HashSet<Card> {
+    [Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades]
+        .into_iter()
+        .flat_map(|suit| cards_for_suit(&suit))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::iter;
+
+    use super::{cards_for_suit, full_deck, Rank, Suit};
+
+    #[test]
+    fn test_rank_in_range_success() {
+        for value in 1..=13u8 {
+            let rank = Rank::new(value);
+            assert_eq!(rank.value(), value)
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_rank_out_of_range_fail() {
+        for value in iter::once(0).chain(14..=u8::MAX) {
+            Rank::new(value);
+        }
+    }
+
+    #[test]
+    fn test_thirteen_cards_for_each_suit() {
+        for suit in [Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades] {
+            let cards = cards_for_suit(&suit);
+            assert_eq!(cards.len(), 13);
+        }
+    }
+
+    #[test]
+    fn test_fifty_two_cards_in_deck() {
+        let deck = full_deck();
+        assert_eq!(deck.len(), 52usize);
+    }
+}
